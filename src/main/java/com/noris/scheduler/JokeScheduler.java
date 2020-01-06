03aa11377046c83020efc9dab753a7a03af20bc3
Aleksandar Stojanovic <astojanovic17@raf.rs>
2020-01-06 19:56:26 +0100

    refactored

--- src/main/java/com/noris/scheduler/JokeScheduler.java
+++ src/main/java/com/noris/scheduler/JokeScheduler.java
@@ -4,10 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.noris.dto.JokeDto;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -39,11 +44,21 @@ public class JokeScheduler {
 
     @Scheduled(initialDelay = 10000, fixedRate = 10000)
     public void getJoke() throws JsonProcessingException {
-        ResponseEntity<JokeDto> matchesDtoResponseEntity = norisApiClient
-                .exchange("https://api.chucknorris.io/jokes/random", HttpMethod.GET, null, JokeDto.class);
-        if (matchesDtoResponseEntity.getStatusCode().equals(HttpStatus.OK))
-            jmsTemplate.convertAndSend(emailQueueDestination, objectMapper
-                    .writeValueAsString(matchesDtoResponseEntity.getBody()));
-        System.out.println(matchesDtoResponseEntity.getBody().getId() + "," + matchesDtoResponseEntity.getBody().getJoke());
+    	
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+            headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+            HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+            ResponseEntity<JokeDto> response = norisApiClient.exchange("https://api.chucknorris.io/jokes/random", HttpMethod.GET,entity,JokeDto.class);
+            if (response.getStatusCode().equals(HttpStatus.OK))
+                jmsTemplate.convertAndSend(emailQueueDestination, objectMapper.writeValueAsString(response.getBody().getValue()));
+
+            System.out.println(response);
+        } catch (Exception ex) {
+           ex.printStackTrace();
+
+        }
     }
 }
